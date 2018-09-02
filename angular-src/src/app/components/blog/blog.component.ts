af61032156679dbac21716c657b078494ad60e50
Bartosz Bukowiec <bartosz.bukowiec@synerise.com>
2018-09-02 15:08:56 +0200

    lepsza zmiana

--- angular-src/src/app/components/blog/blog.component.ts
+++ angular-src/src/app/components/blog/blog.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { BlogService } from '../../services/blog.service';
+import {Observable} from 'rxjs/Rx';
 
 @Component({
   selector: 'app-blog',
@@ -224,6 +225,12 @@ export class BlogComponent implements OnInit {
     });
 
     this.getAllBlogs(); 
+
+    Observable.interval(4000).subscribe(x => {
+      this.getAllBlogs();
+    });
+
+
   }
 
 }
